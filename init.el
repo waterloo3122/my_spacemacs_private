1079693f04751f4d190bda2cdc7e254850cd5db1
pp <pp>
2018-11-27 17:18:20 +0800

    'add'

--- init.el
+++ init.el
@@ -154,8 +154,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro For Powerline"
-                               :size 18 
+   dotspacemacs-default-font '("Ubuntu Mono"
+                               :size  36 
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
@@ -365,7 +365,7 @@ you should place your code here."
   
   (setq org-directory "/Users/pp/Dropbox/org/")
   (setq org-default-notes-file (concat org-directory "/notes.org"))
-  (smetq org-list-allow-alphabetical t)
+  ;; (smetq org-list-allow-alphabetical t)
   (require 'org2blog-autoloads)
   (require 'auth-source)
   (let (credentials)
@@ -419,6 +419,89 @@ you should place your code here."
             "other"
             (format-time-string "%Y-%m-%d" (current-time))))
   (setq org2blog/wp-buffer-format-function 'my-format-function)
+  
+  ;; -*- lexical-binding: t -*-
+
+;; Font settings.
+
+;; Setting English Font
+(add-to-list 'default-frame-alist '(height . 35))
+(add-to-list 'default-frame-alist '(width . 100))
+;; (add-to-list 'default-frame-alist
+;;              (if (string-equal system-type "windows-nt")
+;; 		 '(font . "Inconsolata-18")
+;; 	       '(font . "Inconsolata-14")))
+
+;; 微软雅黑
+;; 冬青黑体简体中文 W3
+;; Hiragino Sans GB W3
+;; 思源黑体 CN Regular
+;; Source Han Sans
+
+;; Old comments:
+;; (set-fontset-font t
+;; 		      'unicode (font-spec :family "Inconsolata" :size 18))
+;; Chinese Font
+;; cjk-misc gb18030 chinese-gbk chinese-gb2312
+;; (set-fontset-font t
+;; 		    'unicode (font-spec :name "Inconsolata" :size 28))
+
+(defun my--set-font (&optional frame)
+  (with-selected-frame (or frame (selected-frame))
+    (if (string-equal system-type "windows-nt")
+	;; 下面是用于Windows的配置。
+	(progn
+	  ;; 设置英文字体为Inconsolata，并指定字号18。
+	  ;; 因为不同操作系统下字体显示的大小不一样(DPI的问题)，所以分开设置。
+	  (set-face-attribute 'default nil :font "Inconsolata 18")
+
+          ;; (add-to-list 'default-frame-alist
+          ;;              '(font . "Inconsolata 18"))
+	  ;; (set-fontset-font "fontset-startup"
+	  ;; 		    'ascii (font-spec :family "Inconsolata" :size 24) nil 'prepend)
+
+	  ;; 给相应的字符集设置中文字体，这里的字体是冬青黑体简体中文 W3。
+	  (dolist (charset '(han cjk-misc chinese-gbk))
+	    (set-fontset-font "fontset-default"
+			      charset (font-spec :family "冬青黑体简体中文 W3")))
+
+	  ;; Emacs 25.2之后的版本无法单独设置符号的字体，这句禁用这个特性恢复原来的行为。
+	  (setq use-default-font-for-symbols nil)
+
+	  ;; 设置默认字体和用于显示特殊符号的字体，如果不需要下面三句可以删除。
+	  (set-fontset-font "fontset-default"
+			    'unicode "Segoe UI Symbol" nil 'append)
+	  (set-fontset-font "fontset-default"
+			    '(#x1F600 . #x1F64F) "Segoe UI Symbol") ; Emoji
+	  (set-fontset-font "fontset-default"
+	  		    '(#xE000 . #xF8FF) "STIX")) ; Private Use Areas
+      ;; 下面是Linux的配置，道理类似。
+      (set-face-attribute
+       'default nil :font "Inconsolata 14")
+      (dolist (charset '(kana han cjk-misc bopomofo))
+	(set-fontset-font "fontset-default"
+			  charset (font-spec :name "Hiragino Sans GB")))))
+
+  ;; 对于特定的符号设置字体，不分操作系统。我的个人爱好，可以删除。
+  (set-fontset-font "fontset-default" '(#x2010 . #x2017) "Symbola") ;; – —
+  (set-fontset-font "fontset-default" '(#x2032 . #x2037) "Symbola") ;; ′ ″ ‴ ‵ ‶ ‷
+
+  (set-fontset-font "fontset-default" '(#x2018 . #x2019) "Inconsolata") ;; ‘’
+  (set-fontset-font "fontset-default" '(#x201c . #x201d) "Inconsolata") ;; “”
+  (set-fontset-font "fontset-default" ?− "Inconsolata") ;; −
+  )
+
+;; 运行一下立即设置字体。
+;; (add-hook 'after-init-hook 'my--set-font)
+(my--set-font)
+
+;; 对于新建的frame应用设置。
+(add-hook 'after-make-frame-functions #'my--set-font)
+
+(defface strike-through
+  '((t :strike-through t))
+  "Basic strike through face."
+  :group 'basic-faces)
 
 
   )
@@ -433,7 +516,7 @@ you should place your code here."
  '(exec-path-from-shell-arguments (quote ("-l")))
  '(package-selected-packages
    (quote
-    (org2blog metaweblog xml-rpc anaconda-mode pythonic skewer-mode simple-httpd json-mode json-snatcher json-reformat multiple-cursors js2-mode dash-functional tern sql-indent csv-mode nginx-mode haml-mode web-completion-data rake minitest inf-ruby hide-comnt smartparens iedit anzu evil goto-chg undo-tree eval-sexp-fu highlight f s diminish projectile pkg-info epl counsel swiper ivy bind-map bind-key packed dash ace-window helm avy helm-core async popup package-build powerline pcre2el spinner hydra parent-mode request flx chinese-pyim-basedict org alert log4e gntp markdown-mode htmlize gitignore-mode git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter flyspell-correct pos-tip flycheck magit magit-popup git-commit with-editor php-mode diff-hl company yasnippet auto-complete define-word yapfify yaml-mode xterm-color ws-butler window-numbering which-key wgrep web-mode web-beautify volatile-highlights visual-regexp-steroids vi-tilde-fringe uuidgen use-package toc-org tiny tide tagedit spacemacs-theme spaceline smex smeargle slim-mode sicp shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe reveal-in-osx-finder restclient restart-emacs rbenv ranger rainbow-mode rainbow-identifiers rainbow-delimiters racket-mode quelpa pyvenv pytest pyenv-mode py-isort pug-mode projectile-rails prodigy popwin pip-requirements phpunit phpcbf php-extras php-auto-yasnippets persp-mode peep-dired pbcopy paradox osx-trash osx-dictionary origami orgit org-projectile org-present org-pomodoro org-plus-contrib org-octopress org-download org-bullets open-junk-file ob-http nodejs-repl neotree mwim multi-term move-text monokai-theme mmm-mode markdown-toc magit-gitflow magit-gh-pulls macrostep lua-mode lorem-ipsum livid-mode live-py-mode lispy linum-relative link-hint less-css-mode launchctl js2-refactor js-doc ivy-hydra info+ indent-guide impatient-mode ido-vertical-mode ibuffer-projectile hy-mode hungry-delete hl-todo hl-anything highlight-parentheses highlight-numbers highlight-indentation help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-github-stars helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag graphviz-dot-mode google-translate golden-ratio gnuplot glsl-mode github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gist gh-md ggtags flyspell-correct-ivy flyspell-correct-helm flycheck-pos-tip flx-ido find-file-in-project fill-column-indicator feature-mode fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu etags-select eshell-z eshell-prompt-extras esh-help enh-ruby-mode engine-mode emmet-mode elisp-slime-nav editorconfig dumb-jump drupal-mode dockerfile-mode docker discover-my-major disaster deft cython-mode counsel-projectile company-web company-tern company-statistics company-c-headers company-auctex company-anaconda column-enforce-mode color-identifiers-mode coffee-mode cmake-font-lock clojure-snippets clj-refactor clean-aindent-mode clang-format cider-eval-sexp-fu chruby chinese-pyim-greatdict chinese-pyim bundler auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-link ace-jump-helm-line ac-ispell 4clojure)))
+    (winum unfill org-category-capture org2blog metaweblog xml-rpc anaconda-mode pythonic skewer-mode simple-httpd json-mode json-snatcher json-reformat multiple-cursors js2-mode dash-functional tern sql-indent csv-mode nginx-mode haml-mode web-completion-data rake minitest inf-ruby hide-comnt smartparens iedit anzu evil goto-chg undo-tree eval-sexp-fu highlight f s diminish projectile pkg-info epl counsel swiper ivy bind-map bind-key packed dash ace-window helm avy helm-core async popup package-build powerline pcre2el spinner hydra parent-mode request flx chinese-pyim-basedict org alert log4e gntp markdown-mode htmlize gitignore-mode git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter flyspell-correct pos-tip flycheck magit magit-popup git-commit with-editor php-mode diff-hl company yasnippet auto-complete define-word yapfify yaml-mode xterm-color ws-butler window-numbering which-key wgrep web-mode web-beautify volatile-highlights visual-regexp-steroids vi-tilde-fringe uuidgen use-package toc-org tiny tide tagedit spacemacs-theme spaceline smex smeargle slim-mode sicp shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe reveal-in-osx-finder restclient restart-emacs rbenv ranger rainbow-mode rainbow-identifiers rainbow-delimiters racket-mode quelpa pyvenv pytest pyenv-mode py-isort pug-mode projectile-rails prodigy popwin pip-requirements phpunit phpcbf php-extras php-auto-yasnippets persp-mode peep-dired pbcopy paradox osx-trash osx-dictionary origami orgit org-projectile org-present org-pomodoro org-plus-contrib org-octopress org-download org-bullets open-junk-file ob-http nodejs-repl neotree mwim multi-term move-text monokai-theme mmm-mode markdown-toc magit-gitflow magit-gh-pulls macrostep lua-mode lorem-ipsum livid-mode live-py-mode lispy linum-relative link-hint less-css-mode launchctl js2-refactor js-doc ivy-hydra info+ indent-guide impatient-mode ido-vertical-mode ibuffer-projectile hy-mode hungry-delete hl-todo hl-anything highlight-parentheses highlight-numbers highlight-indentation help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-github-stars helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag graphviz-dot-mode google-translate golden-ratio gnuplot glsl-mode github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gist gh-md ggtags flyspell-correct-ivy flyspell-correct-helm flycheck-pos-tip flx-ido find-file-in-project fill-column-indicator feature-mode fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu etags-select eshell-z eshell-prompt-extras esh-help enh-ruby-mode engine-mode emmet-mode elisp-slime-nav editorconfig dumb-jump drupal-mode dockerfile-mode docker discover-my-major disaster deft cython-mode counsel-projectile company-web company-tern company-statistics company-c-headers company-auctex company-anaconda column-enforce-mode color-identifiers-mode coffee-mode cmake-font-lock clojure-snippets clj-refactor clean-aindent-mode clang-format cider-eval-sexp-fu chruby chinese-pyim-greatdict chinese-pyim bundler auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-link ace-jump-helm-line ac-ispell 4clojure)))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
