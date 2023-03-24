f282f476f03f6a0e8eb9861eeb9c6abf72c4015a
pp <panyouyi@licaimofang.com>
2023-03-24 12:02:18 +0800

    last commit msg wrong, right is rmeove org2blg,this commit enable org

--- init.el
+++ init.el
@@ -51,7 +51,7 @@ values."
      emacs-lisp
      git
      markdown
-     ;; org
+     org
      ;;     (shell :variables
      ;;       shell-default-height 30
      ;;       shell-default-position 'bottom)
